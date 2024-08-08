dd5650783c68edb71ddaa2a6fddc45cdc15b2a48
Syed Mahad Hussaini <mahadhussaini>
2024-08-08 20:59:57 +0500

    first commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Logo = ({
   src = "https://cdn3.iconfinder.com/data/icons/inficons/512/github.png",
@@ -8,14 +9,17 @@ const Logo = ({
   title = "GitHub Users",
 }) => {
   return (
-    <div className="flex border-b border-gray-500 pb-2 justify-center items-center">
+    <Link
+      to="/"
+      className="flex border-b border-gray-500 pb-2 justify-center items-center"
+    >
       <img
         src={src}
         alt={alt}
         className={`w-${size} h-${size} rounded-full object-cover`}
       />
       <h1 className="text-2xl px-2 first-letter:text-5xl">{title}</h1>
-    </div>
+    </Link>
   );
 };
 
